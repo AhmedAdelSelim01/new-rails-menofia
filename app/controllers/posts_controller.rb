69dc5231a43e9a1737da6f075d4e3576a80049bc
selim <ahmedadelll364@gmail.com>
2026-04-16 21:25:50 +0200

    Implement posts index action and view to display a list of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+
+    def index
+        @counter= 0
+            @posts = [
+        {
+            id: 1,
+            title: "First Post",
+            content: "Hello world!",
+            author: "Ahmed"
+        },
+        {
+            id: 2,
+            title: "Ruby Tips",
+            content: "Learn arrays and hashes.",
+            author: "Sara"
+        },
+        {
+            id: 3,
+            title: "Rails Guide",
+            content: "Follow the official Rails guide.",
+            author: "Omar"
+        }
+        ]
+
+    end 
+
 end
