ac91bd1f28d3daac2acb1ed72723ddcb99a3be39
selim <ahmedadelll364@gmail.com>
2026-04-17 15:07:04 +0200

    feat: add validations for name and email in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    #validations
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true,format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
